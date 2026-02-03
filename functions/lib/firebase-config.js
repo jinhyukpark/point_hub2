36ff884f7ba5eda57739c09fadf527076db27b10
jinhyukpark <jinhyukpark@illunex.com>
2026-02-04 01:52:39 +0900

    내 개인 프로젝트에 배포에 필요한 사항 수정

--- functions/lib/firebase-config.js
+++ functions/lib/firebase-config.js
@@ -38,7 +38,10 @@ const admin = __importStar(require("firebase-admin"));
 // Initialize Firebase Admin once for the entire application
 if (admin.apps.length === 0) {
     admin.initializeApp({
-        databaseURL: 'https://pointhub-ab054-default-rtdb.asia-southeast1.firebasedatabase.app'
+        // [기존 pointhub-ab054 설정 - 이전 시 주석 해제]
+        // databaseURL: 'https://pointhub-ab054-default-rtdb.asia-southeast1.firebasedatabase.app'
+        // [새 point-hub-a9db1 설정]
+        databaseURL: 'https://point-hub-a9db1-default-rtdb.asia-southeast1.firebasedatabase.app'
     });
 }
 exports.rtdb = admin.database();
